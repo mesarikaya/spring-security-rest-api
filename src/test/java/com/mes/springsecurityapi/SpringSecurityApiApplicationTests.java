4e0f23b1862a830fecde0d11e45c917f8ba1dd1d
mesarikaya <mesarikaya@gmail.com>
2020-12-22 15:11:18 +0100

    Initiate project

---
+++ src/test/java/com/mes/springsecurityapi/SpringSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mes.springsecurityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
